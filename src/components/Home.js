a7d02ae11be573def211640331491e9469456afc
Shagun Garg <101324890+SHAGUN1936@users.noreply.github.com>
2025-05-17 12:44:22 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => (
     <p className="text-lg text-center max-w-xl">
       A passionate full-stack developer focused on creating modern, scalable web apps with clean UI and efficient backend logic.
     </p>
-    <a href="https://drive.google.com/file/d/1khgtnnKoUrGcvsAOYV0uK4aEV5h_hY1D/view?usp=sharing" download className="mt-6 bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
+    <a href="https://drive.google.com/file/d/1aUWVafZec8h6zJmCSsNrvTFEgkn2a7uW/view?usp=sharing" download className="mt-6 bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
       Download Resume
     </a>
   </section>
